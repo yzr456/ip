92e6951b374b4e12cb650abc001c6045e65fdd09
Zirui <97959216+yzr456@users.noreply.github.com>
2025-08-20 23:14:42 +0800

    Add support for list

--- src/main/java/MangoBot.java
+++ src/main/java/MangoBot.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MangoBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm MangoBot\n" +
                 " What can I do for you?\n" +
@@ -14,10 +17,22 @@ public class MangoBot {
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                if (tasks.isEmpty()) {
+                    System.out.println("");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(input);
+                System.out.println("____________________________________________________________\n" +
+                        " added: " + input +
+                        "\n____________________________________________________________");
             }
-            System.out.println("____________________________________________________________\n " +
-                    input +
-                    "\n____________________________________________________________");
         }
         sc.close();
     }

c62ad451fc38226aab8e4e866f9c99ddfca0486e
Zirui <97959216+yzr456@users.noreply.github.com>
2025-08-20 23:03:46 +0800

    Add support for echo and bye

--- src/main/java/MangoBot.java
+++ src/main/java/MangoBot.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class MangoBot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm MangoBot\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________\n " +
+                    input +
+                    "\n____________________________________________________________");
+        }
+        sc.close();
     }
 }

e2b2f70fb036450980033c74eb0215ff5f3d28c4
Zirui <97959216+yzr456@users.noreply.github.com>
2025-08-21 22:12:49 +0800

    Add support for Error Handling

--- src/main/java/MangoBot.java
+++ src/main/java/MangoBot.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class MangoBot {
     private static final String LINE = "____________________________________________________________";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MangoException {
         Scanner sc = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>();
         System.out.println(LINE + "\n Hello! I'm MangoBot\n" +
@@ -17,31 +17,35 @@ public class MangoBot {
             } else if (input.equals("list")) {
                 System.out.println(LINE + "\n Here are the tasks in your list:\n" + LINE);
                 if (tasks.isEmpty()) {
-                    System.out.println("");
+                    System.out.println("No tasks have been added yet.");
                 } else {
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println(" " + (i + 1) + "." + tasks.get(i));
                     }
                 }
-            } else if (input.startsWith("mark ")) {
+            } else if (input.startsWith("mark")) {
+                if (input.length() == 4) throw new MangoException("The index of the Task to mark must be specified");
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 Task t = tasks.get(index);
                 t.markAsDone();
                 System.out.println(LINE + "\n Nice! I've marked this task as done:\n   " +
                         t + "\n" + LINE);
-            } else if (input.startsWith("unmark ")) {
+            } else if (input.startsWith("unmark")) {
+                if (input.length() == 6) throw new MangoException("The index of the Task to unmark must be specified");
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 Task t = tasks.get(index);
                 t.markAsNotDone();
                 System.out.println(LINE + "\n OK, I've marked this task as not done yet:\n   " +
                         t + "\n" + LINE);
-            } else if (input.startsWith("todo ")) {
+            } else if (input.startsWith("todo")) {
+                if (input.length() == 4) throw new MangoException("Todo description cannot be empty.");
                 String desc = input.substring(5).trim();
                 Task t = new Todo(desc);
                 tasks.add(t);
                 System.out.println(LINE + "\n Got it. I've added this task:\n   " + t +
                         "\n Now you have " + tasks.size() + " tasks in the list.\n" + LINE);
-            } else if (input.startsWith("deadline ")) {
+            } else if (input.startsWith("deadline")) {
+                if (input.length() == 8) throw new MangoException("Deadline description cannot be empty.");
                 String[] parts = input.substring(9).trim().split(" /by ", 2);
                 String desc = parts[0].trim();
                 String by = parts[1].trim();
@@ -49,10 +53,11 @@ public class MangoBot {
                 tasks.add(t);
                 System.out.println(LINE + "\n Got it. I've added this task:\n   " + t +
                         "\n Now you have " + tasks.size() + " tasks in the list.\n" + LINE);
-            } else if (input.startsWith("event ")) {
+            } else if (input.startsWith("event")) {
+                if (input.length() == 5) throw new MangoException("Event description cannot be empty.");
                 String parts = input.substring(6).trim();
                 int iFrom = parts.indexOf(" /from ");
-                int iTo   = parts.indexOf(" /to ", iFrom + 7);
+                int iTo = parts.indexOf(" /to ", iFrom + 7);
                 String desc = parts.substring(0, iFrom).trim();
                 String from = parts.substring(iFrom + 7, iTo).trim();
                 String to   = parts.substring(iTo + 5).trim();
@@ -61,7 +66,7 @@ public class MangoBot {
                 System.out.println(LINE + "\n Got it. I've added this task:\n   " + t +
                         "\n Now you have " + tasks.size() + " tasks in the list.\n" + LINE);
             } else {
-                System.out.println(LINE + input + LINE);
+                throw new MangoException("Invalid input.");
             }
         }
         sc.close();
